d6a408d8a59ffce676bb7dd59222a0b9747d593c
nkitamuraRealglobe <kitamuranaoki@realglobe.jp>
2024-10-15 05:42:05 +0900

    framework

--- myadd/trdipole.py
+++ myadd/trdipole.py
@@ -1,19 +1,19 @@
-
+from pyscf.ci.cisd import overlap, amplitudes_to_cisdvec
+from pyscf.cc.eom_rccsd import _IMDS, EOMEESinglet
+from pyscf.cc.ccsd import _ChemistsERIs, ao2mo
 
 def dipole_imds():
     return
 
-def matvec_ee():
+def matvec_ee(eom_ee):
+    return
+
+def lambda2cisd():
+    return
+
+def hamr2cisd():
     return
 
 
-def to_cisdvec():
-    return
-
-
-def overlap():
-    return
-
-
-def get_transition_dipole():
+def get_transition_dipole(eom_ee):
     return
