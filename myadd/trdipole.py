f4cee4c0273952c0eabf5bc5558fd2f0a6a99bcb
nkitamuraRealglobe <kitamuranaoki@realglobe.jp>
2024-10-15 06:22:14 +0900

    framework

--- myadd/trdipole.py
+++ myadd/trdipole.py
@@ -1,4 +1,4 @@
-from pyscf.ci.cisd import overlap, amplitudes_to_cisdvec
+from pyscf.ci.cisd import overlap, amplitudes_to_cisdvec, trans_rdm1
 from pyscf.cc.eom_rccsd import _IMDS, EOMEESinglet, EOMEETriplet
 import numpy as np
 
@@ -12,7 +12,9 @@ def get_transition_dipole(mycc, imds, t1, t2, l1, l2, r1, r2):
     vec = matvec(vec)
     hr1, hr2 = eom_cc.vector_to_amplitudes(vec)
     hr_cisd = amplitudes_to_cisdvec(r0, hr1, hr2)
-    trdip = overlap(lamda_cisd, hr_cisd)
+    nmo = mycc._scf.mo_coeff.shape[1]
+    nocc = mycc._scf.mol.nelectron // 2
+    trdip = overlap(lamda_cisd, hr_cisd, nmo, nocc)
     return trdip
 
 
@@ -51,6 +53,29 @@ def run_eomee():
     imds = _IMDS(mycc, eris)
     imds = imds.make_ee()
     return mycc, imds, t1, t2, l1, l2, r1, r2
+
+
+def cisd():
+    from pyscf import gto, scf, ci
+    mol = gto.Mole()
+    mol.verbose = 5
+    mol.unit = 'A'
+    mol.atom = 'O 0 0 0; O 0 0 1.2'
+    mol.basis = 'ccpvdz'
+    mol.build()
+    
+    mf = scf.RHF(mol)
+    mf.verbose = 7
+    mf.scf()
+
+    myci = ci.CISD(mf)
+    myci.nroots = 2
+    es, cs = myci.kernel()
+
+    dm1 = trans_rdm1(myci, cs[0], cs[1])
+    dipole = mol.intor("int1e_r", comp=3)[0]
+    trdm = np.einsum("ij,ij->", dm1, dipole)
+    return
 
 
 def fill_zero(eris):
